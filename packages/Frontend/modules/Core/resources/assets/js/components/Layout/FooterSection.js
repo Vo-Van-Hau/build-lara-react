25b3b33bc116ff09a81b88f5334cc228fac67aed
Mei0805 <thuymaing0805@gmail.com>
2022-09-26 15:11:32 +0700

    homepage

--- packages/Frontend/modules/Core/resources/assets/js/components/Layout/FooterSection.js
+++ packages/Frontend/modules/Core/resources/assets/js/components/Layout/FooterSection.js
@@ -1,10 +1,42 @@
 import { Layout } from 'antd';
+import { Col, Row } from 'antd';
 const { Footer } = Layout;
 
 const FooterSection = ({...props}) => {
     return (
         <>
-             <Footer>Copyright @ 2022</Footer>
+        <Row className='footer_container' >
+            <Col className='footer_section' span={4}>
+                <h5>Heading</h5>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+            </Col>
+            <Col className='footer_section' span={4}>
+                <h5>Heading</h5>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+            </Col>
+            <Col className='footer_section' span={4}>
+                <h5>Heading</h5>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+            </Col>
+            <Col className='footer_section' span={4}>
+                <h5>Heading</h5>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+                <p>Content</p>
+            </Col>
+        </Row>
+        
+             
         </>
     )
 }
--- packages/Frontend/modules/Home/resources/assets/js/Home/components/Contexts/HomeContext.js
+++ packages/Frontend/modules/Home/resources/assets/js/Home/components/Contexts/HomeContext.js
@@ -1,15 +1,15 @@
 import React, { createContext, useReducer } from 'react';
-import { initialState, GroupsReducer } from '../Reducers/GroupsReducer';
+import { initialState, HomeReducer } from '../Reducers/HomeReducer';
 import {
     GET_GROUPS, SET_USER_GROUPS,
     SET_PAGINATION, SET_TABLE_LOADING, MOUTED
 } from '../Dispatch/type';
 
-export const GroupsContext = createContext();
+export const HomeContext = createContext();
 
-const GroupsContextProvicer = ({ children, axios, history, config }) => {
+const HomeContextProvicer = ({ children, axios, history, config }) => {
 
-    const [data, dispatch] = useReducer(GroupsReducer, initialState);
+    const [data, dispatch] = useReducer(HomeReducer, initialState);
 
     /**
      * @author: <vanhau.vo@urekamedia.vn>
@@ -165,10 +165,10 @@ const GroupsContextProvicer = ({ children, axios, history, config }) => {
     };
 
     return (
-        <GroupsContext.Provider value={todoContextData}>
+        <HomeContext.Provider value={todoContextData}>
             { children }
-        </GroupsContext.Provider>
+        </HomeContext.Provider>
     );
 }
 
-export default GroupsContextProvicer;
+export default HomeContextProvicer;
