210702f94d8ed2c92d887ffd395ebd2039cf12f3
Vo Van Hau <hauvo1709@gmail.com>
2022-09-28 12:43:52 +0700

    build Frontend/Checkout/Payment component

--- packages/Frontend/modules/Checkout/resources/assets/js/index.jsx
+++ packages/Frontend/modules/Checkout/resources/assets/js/index.jsx
@@ -1,6 +1,8 @@
 import Checkout from './Checkout';
 import Cart from './Cart';
+import Payment from './Payment';
 
 export default Checkout;
 export { Checkout as checkout }
 export { Cart as cart }
+export { Payment as payment }
