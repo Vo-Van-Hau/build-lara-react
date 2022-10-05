f6cdef2e1d6fbed6a221952073b0089738a461a2
Vo Van Hau <hauvo1709@gmail.com>
2022-10-05 22:42:47 +0700

    testing notifications in laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Notification;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,3 +13,24 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get("/testing-notification", function() {
+    $user = \Modules\Users\Models\Users::find(1);
+    if(!empty($user)) {
+        $user->notify(new \App\Notifications\OrderTrackingStatus($user)); // not oke
+        echo "Done";
+    }
+    exit;
+});
+
+Route::get("/testing-notification/user", function() {
+    $user = \Modules\Users\Models\Users::find(1);
+    if(!empty($user)) {
+        $notifications = $user->notifications;
+        foreach($notifications as $notification) {
+            echo $notification->data["name"] . " - " . $notification->data["email"] . "<br/>";
+        }
+
+    }
+    exit;
+});
